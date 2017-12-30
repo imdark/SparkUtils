da2611723aa7290f0f41791af3a9c28647ca27c3
imdark <mikesgoacademy@gmail.com>
2017-12-29 16:04:52 -0800

    Create MultiSink.scala

---
+++ MultiSink.scala
@@ -0,0 +1,19 @@
+package org.michael.spark.util
+
+
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.execution.streaming.Sink
+import org.apache.spark.storage.StorageLevel
+
+class MultiSink(sinks : Array[Sink], dataPersistenceLevel: Option[StorageLevel] = Some(StorageLevel.MEMORY_AND_DISK)) extends Sink {
+
+  override def toString(): String = "MultiSink"
+
+  override def addBatch(batchId: Long, data: DataFrame): Unit = {
+    match dataPersistenceLevel {
+        case Some(persistenceLevel) => data.cache(persistenceLevel)
+    }
+    
+    sinks.foreach(sink => sink.addBatch(data))
+  }
+}
