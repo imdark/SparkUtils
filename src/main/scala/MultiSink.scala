36dda9f45b802535bc8839fb6e28d9fb9aba5e65
imdark <mikesgoacademy@gmail.com>
2017-12-29 16:57:24 -0800

    Update MultiSink.scala

--- src/main/scala/MultiSink.scala
+++ src/main/scala/MultiSink.scala
@@ -12,6 +12,7 @@ class MultiSink(sinks : Array[Sink], dataPersistenceLevel: Option[StorageLevel] 
   override def addBatch(batchId: Long, data: DataFrame): Unit = {
     dataPersistenceLevel match {
         case Some(persistenceLevel) => data.persist(persistenceLevel)
+        case None => _
     }
     
     sinks.foreach(sink => sink.addBatch(batchId, data))
